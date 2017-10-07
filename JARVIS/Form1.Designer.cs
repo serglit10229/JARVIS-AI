d3fbb46f0ddf309f5bb91d61beb613c6aae62075
Sergei Litovchenko <serglit10229@gmail.com>
2017-10-07 13:52:29 -0700

    Start Version

--- JARVIS/Form1.Designer.cs
+++ JARVIS/Form1.Designer.cs
@@ -3,14 +3,14 @@
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// Требуется переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,20 +20,45 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(80, 101);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            // this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- JARVIS/Form1.cs
+++ JARVIS/Form1.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.Diagnostics;
 
 namespace JARVIS
 {
@@ -15,6 +17,151 @@ namespace JARVIS
         public Form1()
         {
             InitializeComponent();
+        }
+        SpeechSynthesizer s = new SpeechSynthesizer();
+        Boolean wake = true;
+
+
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+            s.SelectVoice("Microsoft Irina Desktop");
+            //s.SpeakAsync("привет");
+            s.Rate = 3;
+            s.SpeakAsync("123");
+
+
+            System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("ru-RU");
+            SpeechRecognitionEngine sre = new SpeechRecognitionEngine(ci);
+            sre.SetInputToDefaultAudioDevice();
+
+            sre.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(sre_SpeechRecognized);
+
+
+            Choices Words = new Choices();
+            Words.Add(new string[] {
+                "привет", "здравствуй","здравствуйте", "добрый день","дратути","дратути","дороу","однако зравствуйте","ку","как дела","как настроение", "че как",
+                "Сколько время","Сколько времяни", "what is today?", "open google", "turn off", "wake up",
+                "restart", "update", "open steam", "close steam",
+            });
+
+
+            GrammarBuilder gb = new GrammarBuilder();
+            gb.Culture = ci;
+            gb.Append(Words);
+
+
+            Grammar g = new Grammar(gb);
+            sre.LoadGrammar(g);
+
+            sre.RecognizeAsync(RecognizeMode.Multiple);
+
+
+
+
+
+            Console.WriteLine("Installed voices -");
+            foreach (InstalledVoice voice in s.GetInstalledVoices())
+            {
+                VoiceInfo info = voice.VoiceInfo;
+                Console.WriteLine(" Voice Name: " + info.Name);
+            }
+        }
+
+        public void say(String h)
+        {
+            s.Speak(h);
+        }
+
+        public void sre_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            String r = e.Result.Text;
+
+            //What you say
+            if (r == "wake up")
+            {
+                say("Good Morning!");
+                wake = true;
+            }
+
+            if (r == "turn off")
+            {
+                say("Ok, sleeping now!");
+                wake = false;
+            }
+
+
+
+
+            //if (wake == true)
+            //{
+
+
+            //What you say
+            if (r == "Открой (ПРОГРАММА)")
+            {
+                //What it says
+                say("Opening App");
+                Process.Start(@"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe");
+            }
+
+
+            //What you say
+            if (r == "close steam")
+            {
+                //What it says
+                say("Closing App");
+                SendKeys.Send("% +{F4}");
+            }
+
+
+
+            /////////////
+            //Приветствия
+            /////////////
+            {
+                //What you say
+                if (r == "привет" || r == "здравствуй" || r == "здравствуйте" || r == "добрый день" || r == "дратути" || r == "дороу" || r == "однако зравствуйте" || r == "ку")
+                {
+                    //What it says
+                    say("Добрый день, хозяин.");
+                }
+
+
+                //What you say
+                if (r == "как дела" || r == "как настроение" || r == "че как")
+                {
+                    //What it says
+                    say("Всё в норме. Состояние материнской платы - отличное. Процессор в порядке. А как у вас?");
+                }
+            }
+
+
+            //What you say
+            if (r == "Сколько времяни" || r == "Сколько время")
+            {
+                //What it says
+                say(DateTime.Now.ToString("h:mm tt"));
+            }
+
+
+            //What you say
+            if (r == "what is today?")
+            {
+                //What it says
+                say(DateTime.Now.ToString("M/d/yyyy"));
+            }
+
+
+            //What you say
+            if (r == "open google")
+            {
+                //What it says
+                say("OK");
+                Process.Start("https://google.com");
+            }
+            //}
         }
     }
 }
